b6ccc71b88593607d2a601951febb495b52081b7
younghoshin <sim6671@naver.com>
2023-10-25 12:01:44 +0900

    :sparkles: first commit

---
+++ backjoon/src/code/Q_1010.java
@@ -0,0 +1,43 @@
+package code;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q_1010 {
+    static final int[][] bridge = new int[30][30];
+
+
+    public static int bridgeCount(int west, int east){
+        if (bridge[west][east]>0){
+            return bridge[west][east];
+        }
+        if(west == east || east == 0){
+            return  bridge[west][east] = 1;
+        }
+        return bridge[west][east] = bridgeCount(west-1,east)+bridgeCount(west-1,east-1);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(reader.readLine());
+
+        for (int i = 0; i < T; i++)
+        {
+            String[] temp = reader.readLine().split(" ");
+
+            int N = Integer.parseInt(temp[0]);
+            int M = Integer.parseInt(temp[1]);
+
+            System.out.println(bridgeCount(M, N));
+        }
+
+        reader.close();
+
+
+
+
+
+    }
+}
